f338630ef25fa388386edfe9c07bee2398716673
adn87 <aaghadieh@gmail.com>
2023-11-04 13:47:14 -0400

    Tkinter_Intro_004
    - working with GUI.
    - created a button and a text box using Tkinter

--- mian.py
+++ mian.py
@@ -1,17 +1,29 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label.config(text="New Text")
 
 
+def button_clicked():
+    print("I've been clicked")
+    my_label.config(text=Input.get())
 
 
+# Button
+button = Button(text="click me", command=button_clicked)
+button.pack()
 
+# Entry
+Input = Entry(width=10)
+Input.pack()
+Input.get()
 
 
 
